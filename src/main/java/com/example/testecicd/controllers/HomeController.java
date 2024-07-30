0532a0dac926ea161f086b0ac59ba8800f663bc8
Tawanna <thol_16@hotmail.com>
2024-07-29 21:31:37 -0300

    home controller

---
+++ src/main/java/com/example/testecicd/controllers/HomeController.java
@@ -0,0 +1,15 @@
+package com.example.testecicd.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/")
+public class HomeController {
+    @GetMapping
+    public ResponseEntity<String> hello(){
+        return ResponseEntity.ok("Hello");
+    }
+}
